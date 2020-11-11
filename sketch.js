a1429f5365f68c75570a2cad1f43b3728b5ed98a
LimunToS <alexalukac@gmail.com>
2020-11-11 11:34:58 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,26 +1,38 @@
 let ptica;
 let prepreka;
 let game = false;
-var scl = 100;
+var scl = 100000;
 var score = 0;
+
+let img;
+function preload() {
+  img = loadImage('niedwedz.png');
+}
 
 function setup() {
   frameRate(60);
-  createCanvas(480, 400);
+  createCanvas(windowWidth, windowHeight);
+  Image(img,0,0);
   ptica = new Bird();
   prepreka = new Prepreka();
+
 }
 
 function draw() {
   background(255);
   fill('green');
-  rect(0,300,width,100);
+  rect(0,500,width,windowHeight-500); //tlo
   
 
   for(let i = 0;i<scl;i++){
+    if(game = ptica.collision(prepreka)){
+      //ptica.V = 0;
+      //ptica.A = 0;
+    }
     prepreka.update();
     ptica.update();
-    game = ptica.gameOver()||ptica.collision(prepreka);
+    
+    ptica.gameOver();
     
   }
   prepreka.show();
@@ -28,6 +40,7 @@ function draw() {
   if(game){
     prepreka.stop();
   }
+  fill(0);
   textSize(32);
   text(score,10,30);
 }
